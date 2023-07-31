7d41f1c808b39a5b55cad5b41b47e2963e59977b
DanielShvart <danielshvart@gmail.com>
2023-07-31 13:10:25 +0300

    Initial commit

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_
 clf = cv2.CascadeClassifier(str(cascade_path))
 
 
+# Distance from the centre of the screen
 def distance(x_coordinate, y_coordinate):
     return math.sqrt(((x_coordinate - 240) ** 2) + ((y_coordinate - 320) ** 2))
 
@@ -48,31 +49,26 @@ while True:
         # Check if the closest face X coordinate is at the middle of the screen
         if closest_x > 330:
             x_on_target = False
-            i = '1'
-            arduino.write(i.encode())
+            arduino.write('1'.encode())
         elif closest_x < 310:
             x_on_target = False
-            i = '2'
-            arduino.write(i.encode())
+            arduino.write('2'.encode())
         else:
             x_on_target = True
 
         # Check if the closest face Y coordinate is at the middle of the screen
         if closest_y > 250:
             y_on_target = False
-            i = '3'
-            arduino.write(i.encode())
+            arduino.write('3'.encode())
         elif closest_y < 230:
             y_on_target = False
-            i = '4'
-            arduino.write(i.encode())
+            arduino.write('4'.encode())
         else:
             y_on_target = True
 
         # Check if current camera position is aiming exactly at a persons face
         if x_on_target & y_on_target:
-            i = '0'
-            arduino.write(i.encode())
+            arduino.write('0'.encode())
 
         cv2.imshow('video', frame)
         time.sleep(0.1)
